e07c721c841b2adb8ab3c898818cf5675490acfa
Eero Aaltonen <eero.aaltonen@vaisala.com>
2020-05-12 17:43:24 +0300

    Source files for the calculator-server example
    Copied from https://github.com/openbmc/sdbusplus/tree/master/example

---
+++ net/poettering/Calculator/client.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+
+namespace sdbusplus
+{
+namespace net
+{
+namespace poettering
+{
+namespace client
+{
+namespace Calculator
+{
+
+static constexpr auto interface = "net.poettering.Calculator";
+
+} // namespace Calculator
+} // namespace client
+} // namespace poettering
+} // namespace net
+} // namespace sdbusplus
+
